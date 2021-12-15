efb70f05659584b5cdf09447fc0c1474599efd8d
TwoFloorLoft <joaoluisespiguinha@gmail.com>
2021-12-15 17:35:46 +0100

    4.5

--- src/api.js
+++ src/api.js
@@ -1,3 +1,11 @@
+/**
+ *
+ * @param {*} events:
+ * The following function should be in the “api.js” file.
+ * This function takes an events array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+ * The Set will remove all duplicates from the array.
+ */
 import NProgress from 'nprogress';
 import { mockData } from './mock-data';
 import axios from 'axios';
